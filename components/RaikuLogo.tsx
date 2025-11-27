97d56f268e2d6b9cd1e5f20f6800b76884bc842b
Abdulqadir Sagir <62840184+abdulqadirsagir@users.noreply.github.com>
2025-11-27 12:24:14 +0100

    feat: Replace SVG logo with image tag
    Switches the Raiku logo component from an inline SVG to an `<img>` tag referencing a static asset. This simplifies the component and allows for easier asset management.

--- components/RaikuLogo.tsx
+++ components/RaikuLogo.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
 export const RaikuLogo: React.FC<{ className?: string }> = ({ className }) => (
-  <svg
-    viewBox="0 0 94 72"
-    fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg"
+  <img
+    src="/logo.png"
+    alt="Raiku Logo"
     className={className}
-    aria-label="Raiku Logo"
-  >
-    <path d="M31.5 12 L46.5 12 L65 36 L50 36 L65 54 L94 54 L79 72 L20 72 L5 54 L34 54 L19 36 L34 36 Z" />
-  </svg>
+  />
 );
